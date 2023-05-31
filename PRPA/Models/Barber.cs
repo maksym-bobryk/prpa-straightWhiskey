bc796e17bb8f4ffb58f933a9b6c9acd797556bc1
i.pavlovskyi <i.pavlovskyi@dragonslake.com>
2023-05-31 23:31:13 +0200

    fixed barber's earnings field type

--- PRPA/Models/Barber.cs
+++ PRPA/Models/Barber.cs
@@ -3,7 +3,7 @@
     public class Barber
     {
         public int BarberId { get; set; }
-        public int Earnings { get; set; }
+        public decimal Earnings { get; set; }
         public int UserID { get; set; }
     }
 }
