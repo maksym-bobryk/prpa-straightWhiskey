3bf49b655919ecd0fbef14d97dfe6a59e5890b5c
Bonj0urr <kvolnykov@gmail.com>
2023-06-02 00:41:50 +0300

    mega commit

--- PRPA/Models/Service.cs
+++ PRPA/Models/Service.cs
@@ -5,7 +5,7 @@
         public int ServiceId { get; set; }
         public string Name { get; set; }
         public int Duration { get; set; }
-        public float Cost { get; set; }
+        public decimal Cost { get; set; }
         public string Description { get; set; }
     }
 }
