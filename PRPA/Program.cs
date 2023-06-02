62b69f15c2ad4bb3324c5205a049952163bd4cd5
Maksym Bobryk <booobryyyk@gmail.com>
2023-06-02 23:44:10 +0300

    Added fetch and fixed cors

--- PRPA/Program.cs
+++ PRPA/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Configuration;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
+builder.Services.AddCors();
 builder.Services.AddScoped<IAdministratorRepository, AdministratorRepository>();
 builder.Services.AddScoped<IAppointmentRepository, AppointmentRepository>();
 builder.Services.AddScoped<IAppointmentServiceRepository, AppointmentServiceRepository>();
@@ -31,6 +32,13 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseCors(build => build
+    .AllowAnyHeader()
+    .AllowAnyMethod()
+    .SetIsOriginAllowed((host) => true)
+    .AllowCredentials()
+);
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
--- src/script.js
+++ src/script.js
@@ -1,12 +1,8 @@
-console.log('Hello');
-
-fetch('https://127.0.0.1:7061/api/User/Get')
-.then(function(response) {
-  return response.json();
-})
-.then(function(data) {
-  console.log(data);
-})
-.catch(function(error) {
-  console.error('Error:', error);
-});
+fetch('https://localhost:7061/api/Barber/')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+  })
+  .catch(error => {
+    console.error(error);
+  });
