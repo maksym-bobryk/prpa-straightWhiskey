dcb2bd743b32345d973851f72cc4c568142a00a1
i.pavlovskyi <i.pavlovskyi@dragonslake.com>
2023-06-03 16:09:53 +0200

    google auth

--- PRPA/Program.cs
+++ PRPA/Program.cs
@@ -3,6 +3,7 @@ using PRPA.RepositoriesInterfaces;
 using PRPA.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Authentication.Google;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -29,8 +30,8 @@ builder.Services.AddAuthentication(options =>
     options.DefaultChallengeScheme = GoogleDefaults.AuthenticationScheme;
 }).AddGoogle(options =>
 {
-    options.ClientId = "[MyGoogleClientId]";
-    options.ClientSecret = "[MyGoogleSecretKey]";
+    options.ClientId = "247608479620-l5ptoru2lrdm2qj93a0lsmtdnibvlohk.apps.googleusercontent.com";
+    options.ClientSecret = "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";
 });
 
 var app = builder.Build();
