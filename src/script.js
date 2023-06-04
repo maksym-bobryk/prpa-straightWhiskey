5c8cc9a751e3091adfedb001b534adef652e6ef6
Maksym Bobryk <booobryyyk@gmail.com>
2023-06-04 04:49:49 +0300

    ended main page

--- src/script.js
+++ src/script.js
@@ -1,11 +1,11 @@
-
-
+const user = null;
 fetch('https://localhost:7061/api/User/')
   .then(response => response.json())
   .then(data => {
     console.log(data);
   })
   .catch(error => {
+    user = { ...data[0] };
     console.error(error);
   });
 
@@ -16,44 +16,88 @@ fetch('https://localhost:7061/api/User/')
   }
 )
 
+  // function handleLogin(resp) {
+  //   console.log(resp.credential); 
+  //   let userObject = jwt_decode(resp.credential);
+    
+  //   let newUser = {
+  //     email: userObject.email,
+  //     name: userObject.name,
+  //     password: null,
+  //     phone: null,
+  //     photo: userObject.picture, 
+  //     role: {
+  //       roleId: 2,
+  //       accessFlags: 2,
+  //     },
+  //   }
+
+  //   fetch('https://localhost:7061/api/User/', {
+  //     method: 'POST', 
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //     },
+  //     body: JSON.stringify(newUser),
+  //   })
+  //   .then(response => {
+  //     if (!response.ok) {
+  //       throw new Error(`HTTP error! status: ${response.status}`);
+  //     }
+      
+  //     return response.json();
+  //   })
+  //   .then(data => console.log('Success:', data))
+  //   .catch((error) => console.error('Error:', error));
+
+  //   console.log(userObject, userObject.name);
+  // }
+
   function handleLogin(resp) {
     console.log(resp.credential); 
     let userObject = jwt_decode(resp.credential);
-    console.log(userObject);
-  }
+    
+    let newUser = {
+      Email: userObject.email,
+      Name: userObject.name,
+      Password: null,
+      Phone: null,
+      Photo: userObject.picture,
+      RoleID: null // Assumed role ID, modify as needed
+    };
+
+    fetch('https://localhost:7061/api/User', {
+      method: 'POST', 
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(user),
+    })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then(data => console.log('Success:', data))
+    .catch((error) => console.error('Error:', error));
+}
+
+function postUserData(user) {
+  fetch('https://localhost:7061/api/User', {
+    method: 'POST', 
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(user),
+  })
+  .then(response => {
+    return response.json();
+  })
+  .then(data => console.log('Success:', data))
+  .catch((error) => console.error('Error:', error));
+}
 
   google.accounts.id.renderButton(
     document.getElementById("auth"),
     { theme: "outline", size: "large" }
   )
-
-  // function signIn() {
-  //   let oath2Endpoint = 'https://accounts.google.com/o/oauth2/v2/auth';
-
-  //   let form = document.createElement('form');
-  //   form.setAttribute('method', 'GET');
-  //   form.setAttribute('action', oath2Endpoint);
-
-  //   let params = {
-  //     'client_id': '247608479620-l5ptoru2lrdm2qj93a0lsmtdnibvlohk.apps.googleusercontent.com',
-  //     'redirect_uri': 'http://127.0.0.1:5500/src/profile.html',
-  //     'response_type': 'token',
-  //     'scope':'https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/youtube.readonly',
-  //     'include_granted_scopes': 'true',
-  //     'state': 'pass-through-value',
-  //   }
-
-  //   for (let p in params) {
-  //     let input = document.createElement('input');
-  //     input.setAttribute('type', 'hidden');
-  //     input.setAttribute('name', p);
-  //     input.setAttribute('value', params[p]);
-  //     form.appendChild(input);
-  //   }
-
-  //   document.body.appendChild(form);
-
-  //   form.submit()
-  // }
-
-  
